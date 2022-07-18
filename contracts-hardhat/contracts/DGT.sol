3b0ddc7dfeb9c8c77f99e526b78bb30081479a6b
Aniket V Patel <aniket.vp@accubits.com>
2022-07-18 18:19:41 +0530

    feat : updated the DGT points smart contract

--- contracts-hardhat/contracts/DGT.sol
+++ contracts-hardhat/contracts/DGT.sol
@@ -4,33 +4,127 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "./MerkleTree.sol";
-
+import "@openzeppelin/contracts-upgradeable/utils/cryptography/MerkleProofUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 /**
  * @title DGTPoints
  */
  
- contract DGT is Initializable, Merkle, ReentrancyGuardUpgradeable {
-
+ contract DGT is Initializable, OwnableUpgradeable, ReentrancyGuardUpgradeable {
+    struct Score{
+        bytes32 root;
+        bytes32[] proof;
+    }
+    mapping (address => uint256) private latestPoint;
+    mapping (address => uint256[]) private timeStamps;
+    mapping (address=> mapping (uint256 => Score)) private userBalance;
     
+    constructor() initializer {
+         __Ownable_init();
+        __ReentrancyGuard_init();
+    }
 
-    //  /**
-    // * @dev Reverts if the user status is not active
-    // */
-    // modifier onlyIfActive(address userAddress) {
-    //     require(isActive[userAddress],"Status of the user is Inactive");
-    //     _;
-    // }
+    /**
+    * @dev Reverts if there is no historical data associated with this account
+    */
+    modifier ifExist(address _beneficiary)
+    {
+        require(timeStamps[_beneficiary].length > 0, "User Info Not Found");
+        _;
+    }
 
-    // function initialize(
-    //     address token_
-    // ) external initializer{
-    //     require(token_ != address(0x0));    
-    //     _token = IERC20(token_);
-    //     __Administered_init();
-    //     __ReentrancyGuard_init();
-    // }
-    
+    /**
+     * @dev Updates the Points for a user
+     * @param _beneficiary address of the user
+     * @param _root root hash of the merkle tree generated
+     * @param _proof bytes32 array of the merkle proof
+     * @param _timestamp timestamp for the balance updation
+     */
+    function updatePoints(address _beneficiary ,bytes32 _root, bytes32[] memory _proof, uint256 _timestamp)
+        external
+        onlyOwner
+       {
+        require(_timestamp > latestPoint[_beneficiary], "Time Mismatch. Could not update previous points");
 
-   
+        userBalance[_beneficiary][_timestamp].root = _root;
+        userBalance[_beneficiary][_timestamp].proof = _proof;
+        timeStamps[_beneficiary].push(_timestamp);
+        latestPoint[_beneficiary] = _timestamp;
+    }
+
+    /**
+    * @dev Verifies the Current DGT Balance for a given account.
+    * @return the verification status
+    */
+    function verifyCurrentBalance(address _beneficiary, uint256 _points) 
+    external 
+    view 
+    ifExist(_beneficiary)
+    returns (bool){
+        uint256 previousTime = latestPoint[_beneficiary];
+        bytes32 leaf = keccak256(abi.encodePacked(_points));
+        return (MerkleProofUpgradeable.verify(userBalance[_beneficiary][previousTime].proof, userBalance[_beneficiary][previousTime].root, leaf));
+    }
+
+    /**
+    * @dev Verifies the Historical DGT Balance for a given account for any historical time period.
+    * @return the verification status
+    */
+    function verifyHistoricalBalance(address _beneficiary, uint256 _timestamp, uint256 _points) 
+    external 
+    view 
+    ifExist(_beneficiary)
+    returns (bool){
+        uint256 previousTime = getPreviousTime(timeStamps[_beneficiary],_timestamp);
+        bytes32 leaf = keccak256(abi.encodePacked(_points));
+        return (MerkleProofUpgradeable.verify(userBalance[_beneficiary][previousTime].proof, userBalance[_beneficiary][previousTime].root, leaf));
+    }
+
+    /**
+    * @dev Gives the total numbers of historical datas for a user
+    * @return Historical DGT counts
+    */
+    function getBeneficiariesHistoryCount(address _beneficiary)
+    external
+    view
+    returns(uint256){
+        return timeStamps[_beneficiary].length;
+    }
+
+    function isAfter (uint256 _current, uint256 _previous)
+     pure
+     private
+     returns (bool){
+        if(_current >= _previous) return true;
+        return false;
+    }
+
+    function getLargest(uint256[] memory _timeStampArray) 
+    pure
+    private 
+    returns(uint256){
+       uint store_var = 0;
+       uint i;
+       for(i=0;i<_timeStampArray.length;i++){
+           if(store_var<_timeStampArray[i]){
+               store_var = _timeStampArray[i];
+           }
+       }
+       return store_var;
+   }
+
+    function getPreviousTime (uint256[] memory _timeStampArray, uint256 _inputTime)
+     pure
+     private
+     returns (uint256 previousTime){
+        uint256[] memory tempTimeArray = new uint[](_timeStampArray.length);
+        uint count = 0;
+        for(uint i=0; i<_timeStampArray.length; i++){
+            if(isAfter(_inputTime,_timeStampArray[i])){
+                tempTimeArray[count] = _timeStampArray[i];
+                count ++;
+            }
+        }
+        previousTime = getLargest(tempTimeArray);
+    } 
  }
