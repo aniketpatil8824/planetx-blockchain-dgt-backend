b42c7d2d55824377b28c17d92a6479642c7da7a4
Aniket V Patel <aniket.vp@accubits.com>
2022-07-18 11:41:16 +0530

    feat: DGT contract initial commit

---
+++ contracts-hardhat/contracts/DGT.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "./MerkleTree.sol";
+
+/**
+ * @title DGTPoints
+ */
+ 
+ contract DGT is Initializable, Merkle, ReentrancyGuardUpgradeable {
+
+    
+
+    //  /**
+    // * @dev Reverts if the user status is not active
+    // */
+    // modifier onlyIfActive(address userAddress) {
+    //     require(isActive[userAddress],"Status of the user is Inactive");
+    //     _;
+    // }
+
+    // function initialize(
+    //     address token_
+    // ) external initializer{
+    //     require(token_ != address(0x0));    
+    //     _token = IERC20(token_);
+    //     __Administered_init();
+    //     __ReentrancyGuard_init();
+    // }
+    
+
+   
+ }
