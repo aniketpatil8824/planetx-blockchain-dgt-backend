5a4d7c9809a763fb4a89e9e607d16c8ddc16d9ec
Aniket <aniket.vp@accubits.com>
2022-09-06 15:25:39 +0530

    Modified code

---
+++ contracts-hardhat/contracts/PlanetXTimelockController.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "@openzeppelin/contracts-upgradeable/governance/TimelockControllerUpgradeable.sol";
+import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
+import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
+
+contract PlanetXTimelockController is
+    TimelockControllerUpgradeable,
+    ERC721Holder,
+    ERC1155Holder
+{
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() initializer {}
+
+    function initialize(
+        uint256 minDelay,
+        address[] memory proposers,
+        address[] memory executors
+    ) public initializer {
+        __TimelockController_init(minDelay, proposers, executors);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(TimelockControllerUpgradeable, ERC1155Receiver, onERC1155BatchReceived, onERC721Received)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
