4d6a69e652d6c819c0652be336260b9a7af54ca2
mbcse <mbcse50@gmail.com>
2022-11-17 12:05:54 +0530

    swagger example added

--- routes/admin/account.js
+++ routes/admin/account.js
@@ -3,6 +3,34 @@ import { createAccount, getAccount } from '../../controllers/admin'
 
 const router = express.Router()
 
+/**
+ * @swagger
+ *  components:
+ *             securitySchemes:
+ *              ApiKeyAuth:
+ *                 type: apiKey
+ *                 in: header
+ *                 name: X-API-KEY
+ * /admin/payout/:
+ *      post:
+ *          description: Payout USDC to a Address using circle
+ *
+ *          security:
+ *             - ApiKeyAuth: []
+ *          parameters:
+ *              - name: reciepientAddress
+ *                description: Metmask Address of User to whom payout is to be done
+ *                required: true
+ *                type: string
+ *              - name: amount
+ *                description: "amount in usdc to be sent"
+ *                required: true
+ *                type: Number
+ *          responses:
+ *              200:
+ *                description: Success
+ *
+ */
 router.get('/', getAccount)
 router.post('/create', createAccount)
 
