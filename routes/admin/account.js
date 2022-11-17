3d8b16492384e91366a0b31fdc912856f37b7f52
mbcse <mbcse50@gmail.com>
2022-11-17 12:29:28 +0530

    New Folders

--- routes/admin/account.js
+++ routes/admin/account.js
@@ -32,6 +32,35 @@ const router = express.Router()
  *
  */
 router.get('/', getAccount)
+
+/**
+ * @swagger
+ *  components:
+ *             securitySchemes:
+ *              ApiKeyAuth:
+ *                 type: apiKey
+ *                 in: header
+ *                 name: X-API-KEY
+ * /create:
+ *      post:
+ *          description: Payout USDC to a Address using circle
+ *
+ *          security:
+ *             - ApiKeyAuth: []
+ *          parameters:
+ *              - name: reciepientAddress
+ *                description: Metmask Address of User to whom payout is to be done
+ *                required: true
+ *                type: string
+ *              - name: amount
+ *                description: "amount in usdc to be sent"
+ *                required: true
+ *                type: Number
+ *          responses:
+ *              200:
+ *                description: Success
+ *
+ */
 router.post('/create', createAccount)
 
 export default router
